0f11e668c900781e86b99944db5f9354980ef52d
Hanzohanic <hanzohanic@gmail.com>
2022-11-22 14:02:42 +0200

    v1

---
+++ set_env.sh
@@ -0,0 +1,12 @@
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=db.simba03
+export POSTGRES_HOST=database-1.cewjucffttvt.us-east-2.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::project3-762005184295-bucket
+export AWS_REGION=us-east-2
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
+
+
+
